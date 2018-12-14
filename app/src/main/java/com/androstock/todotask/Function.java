adb02ed5bfde2babff9f618bce8e4df6b60b27f5
Harshit Bhatia <harshitbhatia28698@gmail.com>
2018-12-14 16:26:06 +0530

    Final Commit

---
+++ app/src/main/java/com/androstock/todotask/Function.java
@@ -0,0 +1,24 @@
+package com.androstock.todotask;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Function {
+
+    public static String Epoch2DateString(String epochSeconds, String formatString) {
+        Date updatedate = new Date(Long.parseLong(epochSeconds));
+        SimpleDateFormat format = new SimpleDateFormat(formatString);
+        return format.format(updatedate);
+    }
+
+
+    public static Calendar Epoch2Calender(String epochSeconds) {
+        Date updatedate = new Date(Long.parseLong(epochSeconds));
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(updatedate);
+
+        return cal;
+    }
+
+}
